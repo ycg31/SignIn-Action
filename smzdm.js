585fef2da174c535240000d69f087550ac9a5947
ycg31 <ycg31@qq.com>
2021-01-02 16:57:40 +0800

    Update smzdm.js

--- smzdm.js
+++ smzdm.js
@@ -135,7 +135,7 @@ function showmsg() {
       $.subt += ', APP: 设置签到账号'
     }
     //$.msg($.name, $.subt, $.desc.join('\n'))
-    await notify.sendNotify(`${$.name} ', '${$.subt}`, `${$.desc.join} `);
+    notify.sendNotify(`${$.name} ', '${$.subt}`, `${$.desc.join} `);
 
     resolve()
   })
