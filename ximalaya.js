5af4f182f543569d37593f8ffcaae084a3946bc9
yandong31 <yandong31@gmail.com>
2021-01-06 20:13:09 +0800

    edit ximalaya.js

--- ximalaya.js
+++ ximalaya.js
@@ -4,7 +4,7 @@ const notify = require('./sendNotify');
 // 判断github action里面是否有值得买cookies
 if (process.env.XMLY_COOKIES) {
   $.VAL_cookies = process.env.XMLY_COOKIES
-else{
+}else{
   notify.sendNotify('喜马拉雅', '未设置cookie', '请检查secret里是否设置XMLY_COOKIES');
 }
 
