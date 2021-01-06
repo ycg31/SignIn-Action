78e037902e00e59db7f8a5060a054d31ad8ec3de
yandong31 <yandong31@gmail.com>
2021-01-06 22:58:44 +0800

    修改v2ex通知

--- v2exsign.js
+++ v2exsign.js
@@ -10,16 +10,17 @@ mod by ycg31
 4.在github仓库的settings-secrets里面新建一个，name为V2EXCK，Vaule为上面获取的cookie.
 */
 
+const notify = require('./sendNotify');
+const fs = require("fs");
+const axios = require("axios");
+const moment = require('moment');
+
 // 判断github action里面是否有饿了么cookies
 if (!process.env.V2EXCK) {
   notify.sendNotify('V2EX', '未设置cookie', '请检查secret里是否设置V2EXCK');  
   process.exitCode = 1; 
 }
 const cookie = process.env.V2EXCK;
-const notify = require('./sendNotify');
-const fs = require("fs");
-const axios = require("axios");
-const moment = require('moment');
 
 once = null;
 ckstatus = 1; 
@@ -30,6 +31,7 @@ time = moment().utcOffset(8).format('YYYY-MM-DD HH:mm:ss');
 //tmpHours = time.getHours();time.setHours(tmpHours + 8);
 //notice = time.toLocaleString() + "\n";
 notice = time + "\n";
+title = "V2EX签到："
 const header = {
     headers: {
         Referer: "https://www.v2ex.com/mission",
@@ -50,10 +52,12 @@ function check() {
                 console.log("cookie失效");
                 ckstatus = 0;
                 notice += "cookie失效";
+                title += "cookie失效";
             } else {
                 reg = /每日登录奖励已领取/;
                 if (reg.test(res.data)) {
                     notice += "今天已经签到过啦\n";
+                    title += "重复";
                     signstatus =1 ;
                 } else {
                     reg = /redeem\?once=(.*?)'/;
@@ -77,9 +81,11 @@ function daily() {
             reg = /已成功领取每日登录奖励/;
             if (reg.test(res.data)) {
                 notice += "签到成功\n";
+                title += "成功";
                 signstatus =1 ;
             } else {
                 notice += "签到失败\n";
+                title += "失败";
             }
         } catch (err) {
             console.log(err);
@@ -109,7 +115,7 @@ function sign() {
         try {
           if (!cookie) {
               console.log("你的cookie呢！！！");
-              notify.sendNotify("V2EX", "你的cookie呢！！！", "");
+//              notify.sendNotify("V2EX", "你的cookie呢！！！", "");
               return;
           }
 
@@ -138,7 +144,7 @@ function sign() {
                   }
               }
           );
-          await notify.sendNotify("V2EX", notice, "");
+          await notify.sendNotify(title, notice, "");
         } catch (err) {
             console.log(err);
         }
